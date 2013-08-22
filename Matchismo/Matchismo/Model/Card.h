f05b6059e9beca0a97f995b2aff451c6c0e2eda6
Andrei-Lucian Șerb <lucian.serb@icloud.com>
2013-08-22 20:57:17 +0300

    Add Card model class
    Instances of this class represent a generic card.

---
+++ Matchismo/Matchismo/Model/Card.h
@@ -0,0 +1,13 @@
+//
+//  Card.h
+//  Matchismo
+//
+//  Created by Andrei-Lucian Șerb on 8/22/13.
+//  Copyright (c) 2013 Andrei-Lucian Șerb. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Card : NSObject
+
+@end
---
+++ Matchismo/Matchismo/Model/Card.m
@@ -0,0 +1,13 @@
+//
+//  Card.m
+//  Matchismo
+//
+//  Created by Andrei-Lucian Șerb on 8/22/13.
+//  Copyright (c) 2013 Andrei-Lucian Șerb. All rights reserved.
+//
+
+#import "Card.h"
+
+@implementation Card
+
+@end
