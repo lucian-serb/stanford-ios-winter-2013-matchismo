103d23accaa8b0f743ef4b4716c455c83d41cefa
Andrei-Lucian Șerb <lucian.serb@icloud.com>
2013-08-22 22:15:10 +0300

    Add the PlayingCard class extension of Card to the model

---
+++ Matchismo/Matchismo/Model/PlayingCard.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCard.h
+//  Matchismo
+//
+//  Created by Andrei-Lucian Șerb on 8/22/13.
+//  Copyright (c) 2013 Andrei-Lucian Șerb. All rights reserved.
+//
+
+#import "Card.h"
+
+@interface PlayingCard : Card
+
+@end
---
+++ Matchismo/Matchismo/Model/PlayingCard.m
@@ -0,0 +1,13 @@
+//
+//  PlayingCard.m
+//  Matchismo
+//
+//  Created by Andrei-Lucian Șerb on 8/22/13.
+//  Copyright (c) 2013 Andrei-Lucian Șerb. All rights reserved.
+//
+
+#import "PlayingCard.h"
+
+@implementation PlayingCard
+
+@end
