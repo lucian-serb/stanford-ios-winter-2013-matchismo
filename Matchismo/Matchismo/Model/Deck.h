a0991ea504cac00e75bc84c8af44912ed8ed853d
Andrei-Lucian Șerb <lucian.serb@icloud.com>
2013-08-22 21:00:14 +0300

    Add the Deck model class
    Instances of this class represent a deck of generic cards.

---
+++ Matchismo/Matchismo/Model/Deck.h
@@ -0,0 +1,13 @@
+//
+//  Deck.h
+//  Matchismo
+//
+//  Created by Andrei-Lucian Șerb on 8/22/13.
+//  Copyright (c) 2013 Andrei-Lucian Șerb. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Deck : NSObject
+
+@end
---
+++ Matchismo/Matchismo/Model/Deck.m
@@ -0,0 +1,13 @@
+//
+//  Deck.m
+//  Matchismo
+//
+//  Created by Andrei-Lucian Șerb on 8/22/13.
+//  Copyright (c) 2013 Andrei-Lucian Șerb. All rights reserved.
+//
+
+#import "Deck.h"
+
+@implementation Deck
+
+@end
