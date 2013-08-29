604c3601b5884af9b8df946e24b7f66495890c9d
Andrei-Lucian Șerb <lucian.serb@icloud.com>
2013-08-29 16:35:46 +0300

    Add the CardMatchingGame model class
    Instances of this class represent the card matching game logic.

---
+++ Matchismo/Matchismo/Model/CardMatchingGame.h
@@ -0,0 +1,13 @@
+//
+//  CardMatchingGame.h
+//  Matchismo
+//
+//  Created by Andrei-Lucian Șerb on 8/29/13.
+//  Copyright (c) 2013 Andrei-Lucian Șerb. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CardMatchingGame : NSObject
+
+@end
---
+++ Matchismo/Matchismo/Model/CardMatchingGame.m
@@ -0,0 +1,13 @@
+//
+//  CardMatchingGame.m
+//  Matchismo
+//
+//  Created by Andrei-Lucian Șerb on 8/29/13.
+//  Copyright (c) 2013 Andrei-Lucian Șerb. All rights reserved.
+//
+
+#import "CardMatchingGame.h"
+
+@implementation CardMatchingGame
+
+@end
