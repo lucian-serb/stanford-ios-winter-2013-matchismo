3b162b2b5e97d358734f55f64b68f44a12539ddf
Andrei-Lucian Șerb <lucian.serb@icloud.com>
2013-08-31 21:40:43 +0300

    Change the instantiation of the game propriety, since the instantiation method has changed

--- Matchismo/Matchismo/CardGameViewController.m
+++ Matchismo/Matchismo/CardGameViewController.m
@@ -89,6 +89,7 @@
 {
     if (!_game) {
         _game = [[CardMatchingGame alloc] initWithCardCount:[self.cardButtons count]
+                                            noMatchingCards:self.gameMode
                                                   usingDeck:[[PlayingCardDeck alloc] init]];
     }
     
