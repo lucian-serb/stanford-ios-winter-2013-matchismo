1e8c656b27737ac7e83c79401333eb3b68d169be
Andrei-Lucian Șerb <lucian.serb@icloud.com>
2013-09-06 15:58:47 +0300

    Add GameResult class as helper for showing the game results

---
+++ Matchismo/Matchismo/GameResults.h
@@ -0,0 +1,13 @@
+//
+//  GameResults.h
+//  Matchismo
+//
+//  Created by Andrei-Lucian Șerb on 9/6/13.
+//  Copyright (c) 2013 Andrei-Lucian Șerb. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GameResults : NSObject
+
+@end
---
+++ Matchismo/Matchismo/GameResults.m
@@ -0,0 +1,13 @@
+//
+//  GameResults.m
+//  Matchismo
+//
+//  Created by Andrei-Lucian Șerb on 9/6/13.
+//  Copyright (c) 2013 Andrei-Lucian Șerb. All rights reserved.
+//
+
+#import "GameResults.h"
+
+@implementation GameResults
+
+@end
