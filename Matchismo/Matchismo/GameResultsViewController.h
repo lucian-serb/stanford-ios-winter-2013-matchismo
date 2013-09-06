37c8eaa92b311df68c41ee6fd1c22a950659ff03
Andrei-Lucian Șerb <lucian.serb@icloud.com>
2013-09-06 14:47:07 +0300

    Add a new scene to our application that will be used to display the scores in the game

---
+++ Matchismo/Matchismo/GameResultsViewController.h
@@ -0,0 +1,13 @@
+//
+//  GameResultsViewController.h
+//  Matchismo
+//
+//  Created by Andrei-Lucian Șerb on 9/6/13.
+//  Copyright (c) 2013 Andrei-Lucian Șerb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameResultsViewController : UIViewController
+
+@end
