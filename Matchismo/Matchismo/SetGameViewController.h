ee40f3025c3c96be9d7221e6c49cbbc3f832c234
Andrei-Lucian Șerb <lucian.serb@icloud.com>
2013-09-12 11:02:41 +0300

    Add a ViewController for the Set Game view

---
+++ Matchismo/Matchismo/SetGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  SetGameViewController.h
+//  Matchismo
+//
+//  Created by Andrei-Lucian Șerb on 9/12/13.
+//  Copyright (c) 2013 Andrei-Lucian Șerb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SetGameViewController : UIViewController
+
+@end
