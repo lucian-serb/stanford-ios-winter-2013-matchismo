7fc775767df7da5ef906d4ecfc6ce0559dc2d3b3
Andrei-Lucian Șerb <lucian.serb@icloud.com>
2013-09-12 13:15:02 +0300

    Add a Set card class to the model

---
+++ Matchismo/Matchismo/Model/SetCard.h
@@ -0,0 +1,13 @@
+//
+//  SetCard.h
+//  Matchismo
+//
+//  Created by Andrei-Lucian Șerb on 9/12/13.
+//  Copyright (c) 2013 Andrei-Lucian Șerb. All rights reserved.
+//
+
+#import "Card.h"
+
+@interface SetCard : Card
+
+@end
---
+++ Matchismo/Matchismo/Model/SetCard.m
@@ -0,0 +1,13 @@
+//
+//  SetCard.m
+//  Matchismo
+//
+//  Created by Andrei-Lucian Șerb on 9/12/13.
+//  Copyright (c) 2013 Andrei-Lucian Șerb. All rights reserved.
+//
+
+#import "SetCard.h"
+
+@implementation SetCard
+
+@end
