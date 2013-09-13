72938d8410494dd772cda359ffa16226c5ca7930
Andrei-Lucian Șerb <lucian.serb@icloud.com>
2013-09-13 12:09:50 +0300

    Overwrite the init method of SetCardDeck to return a deck full of the Set Game cards

--- Matchismo/Matchismo/Model/SetCardDeck.m
+++ Matchismo/Matchismo/Model/SetCardDeck.m
@@ -7,7 +7,32 @@
 //
 
 #import "SetCardDeck.h"
+#import "SetCard.h"
 
 @implementation SetCardDeck
+
+- (id)init
+{
+    self = [super init];
+    
+    if (self) {
+        for (NSString *symbol in [SetCard validSymbols]) {
+            for (NSNumber *color in [SetCard validColors]) {
+                for (NSNumber *shading in [SetCard validShading]) {
+                    for (NSUInteger i = 1; i <= [SetCard maxSymbols]; i++) {
+                        SetCard *card = [[SetCard alloc] init];
+                        card.symbol = symbol;
+                        card.color = [color unsignedIntegerValue];
+                        card.shading = [shading unsignedIntegerValue];
+                        card.nrSymbols = i;
+                        [self addCard:card atTop:NO];
+                    }
+                }
+            }
+        }
+    }
+    
+    return self;
+}
 
 @end
