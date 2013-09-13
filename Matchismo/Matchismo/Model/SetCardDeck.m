a8de4cb83485d6611ddf2dc7f18de023db276ff4
Andrei-Lucian Șerb <lucian.serb@icloud.com>
2013-09-13 11:43:54 +0300

    Add a new SetCardDeck class to the model
    This class represents a deck of Set Game cards.

---
+++ Matchismo/Matchismo/Model/SetCardDeck.m
@@ -0,0 +1,13 @@
+//
+//  SetCardDeck.m
+//  Matchismo
+//
+//  Created by Andrei-Lucian Șerb on 9/13/13.
+//  Copyright (c) 2013 Andrei-Lucian Șerb. All rights reserved.
+//
+
+#import "SetCardDeck.h"
+
+@implementation SetCardDeck
+
+@end
