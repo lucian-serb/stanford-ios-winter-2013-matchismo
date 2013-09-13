3991e515e9b9c4f2827c2edef5bc840ba6c480c2
Andrei-Lucian Șerb <lucian.serb@icloud.com>
2013-09-13 13:13:59 +0300

    Add SetGame class to the model
    This class represents the logic for the game of Set.

---
+++ Matchismo/Matchismo/Model/SetGame.h
@@ -0,0 +1,13 @@
+//
+//  SetGame.h
+//  Matchismo
+//
+//  Created by Andrei-Lucian Șerb on 9/13/13.
+//  Copyright (c) 2013 Andrei-Lucian Șerb. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SetGame : NSObject
+
+@end
---
+++ Matchismo/Matchismo/Model/SetGame.m
@@ -0,0 +1,13 @@
+//
+//  SetGame.m
+//  Matchismo
+//
+//  Created by Andrei-Lucian Șerb on 9/13/13.
+//  Copyright (c) 2013 Andrei-Lucian Șerb. All rights reserved.
+//
+
+#import "SetGame.h"
+
+@implementation SetGame
+
+@end
