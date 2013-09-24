31ac5d1cdbbc304e56bb16c5dc44d7c5c0881fc0
Andrei-Lucian Șerb <lucian.serb@icloud.com>
2013-09-24 16:51:26 +0300

    Add GameResults class that handles the game results
    The public interface allows getting the results sorted by date,
    duration, score, or not sorted. We can read the start time, stop time
    and the duration for a game, and we can read/write the score of a game.

---
+++ Matchismo/Matchismo/GameResults.h
@@ -0,0 +1,23 @@
+//
+//  GameResults.h
+//  Matchismo
+//
+//  Created by Andrei-Lucian Șerb on 9/24/13.
+//  Copyright (c) 2013 Andrei-Lucian Șerb. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GameResults : NSObject
+
++ (NSArray *)allGamesResults;
++ (NSArray *)allGamesResultsSortedByDate;
++ (NSArray *)allGamesResultsSortedByDuration;
++ (NSArray *)allGamesResultsSortedByScore;
+
+@property (readonly, nonatomic) NSDate *start;
+@property (readonly, nonatomic) NSDate *stop;
+@property (readonly, nonatomic) NSTimeInterval duration;
+@property (nonatomic) NSInteger score;
+
+@end
