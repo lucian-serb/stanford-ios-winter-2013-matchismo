800a3533cb901e72b2016861cf26d7b738ffad76
Andrei-Lucian Șerb <lucian.serb@icloud.com>
2013-09-24 17:45:49 +0300

    Add action methods for the sort buttons

--- Matchismo/Matchismo/GameResultsViewController.m
+++ Matchismo/Matchismo/GameResultsViewController.m
@@ -91,4 +91,23 @@ typedef enum {BY_DATE, BY_DURATION, BY_SCORE, ENUM_NR_ITEMS} SORT_TYPE_T;
         _sortType = BY_DATE;
     }
 }
+
+- (IBAction)sortByDate
+{
+    self.sortType = BY_DATE;
+    [self updateUI];
+}
+
+- (IBAction)sortByDuration
+{
+    self.sortType = BY_DURATION;
+    [self updateUI];
+}
+
+- (IBAction)sortByScore
+{
+    self.sortType = BY_SCORE;
+    [self updateUI];
+}
+
 @end
