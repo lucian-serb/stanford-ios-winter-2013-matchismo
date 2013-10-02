aee74ba6f2dfca181c023434f78cf09edb947e2a
Andrei-Lucian Șerb <lucian.serb@icloud.com>
2013-10-02 15:22:07 +0300

    Create a class for the setting of the app

---
+++ Matchismo/Matchismo/Settings.h
@@ -0,0 +1,13 @@
+//
+//  Settings.h
+//  Matchismo
+//
+//  Created by Andrei-Lucian Șerb on 10/2/13.
+//  Copyright (c) 2013 Andrei-Lucian Șerb. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Settings : NSObject
+
+@end
---
+++ Matchismo/Matchismo/Settings.m
@@ -0,0 +1,13 @@
+//
+//  Settings.m
+//  Matchismo
+//
+//  Created by Andrei-Lucian Șerb on 10/2/13.
+//  Copyright (c) 2013 Andrei-Lucian Șerb. All rights reserved.
+//
+
+#import "Settings.h"
+
+@implementation Settings
+
+@end
