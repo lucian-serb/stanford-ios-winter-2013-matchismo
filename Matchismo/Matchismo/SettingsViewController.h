db868754d826a334447d3af1375a94e3d991b61c
Andrei-Lucian Șerb <lucian.serb@icloud.com>
2013-10-02 11:20:53 +0300

    Add a new view + view controller for the settings of this app

---
+++ Matchismo/Matchismo/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  Matchismo
+//
+//  Created by Andrei-Lucian Șerb on 10/2/13.
+//  Copyright (c) 2013 Andrei-Lucian Șerb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
